26a24cd60debb9874c08ae3c705473e83ccd8549
MistDiamond075 <161460156+MistDiamond075@users.noreply.github.com>
2024-11-12 09:36:46 +0500

    Add files via upload

---
+++ src/main/resources/static/videoplayer/src/js/plyr.polyfilled.js
@@ -0,0 +1,13 @@
+// ==========================================================================
+// Plyr Polyfilled Build
+// plyr.js v3.7.8
+// https://github.com/sampotts/plyr
+// License: The MIT License (MIT)
+// ==========================================================================
+
+import 'custom-event-polyfill.js';
+import 'url-polyfill.js';
+
+import Plyr from './plyr.js';
+
+export default Plyr;
